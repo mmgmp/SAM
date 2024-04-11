fc5cbf9ab3d3f5f084263ce0a74973018850855a
Marcos <103284514+mmgmp@users.noreply.github.com>
2024-04-11 10:55:26 +0200

    Add files via upload

---
+++ ly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Instalar dependencias
+sudo apt install -y build-essential libpam0g-dev libxcb-xkb-dev
+
+# Clonar repositorio
+git clone --recurse-submodules https://github.com/fairyglade/ly
+
+# Compilar ly
+cd ly/
+make
+sudo make install installsystemd
+
+# Activar ly en systemd
+systemctl enable ly.service
+
---
+++ sway.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Instalar Sway para Debian 12
+sudo apt install -y sway
+
+# Instalar ejecuro de programas
+sudo apt install -y tofi
