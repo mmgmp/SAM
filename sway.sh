17eda18b6807fee6db0129d45f6901175f824e73
Marcos <103284514+mmgmp@users.noreply.github.com>
2024-04-11 11:12:13 +0200

    Add files via upload

--- sway.sh
+++ sway.sh
@@ -3,5 +3,12 @@
 # Instalar Sway para Debian 12
 sudo apt install -y sway
 
-# Instalar ejecuro de programas
-sudo apt install -y tofi
+# Instalar programas adicionales
+sudo apt install -y waybar tofi firefox-esr lf
+
+# Descargar dotfiles de github
+git clone https://github.com/mmgmp/dotfiles
+
+# Colocar archivos config
+mkdir ~/.config/sway
+cp dotfiles/sway/config ~/.config/sway/config
