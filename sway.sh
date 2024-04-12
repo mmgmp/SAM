441b17a3ec5d2c8fc8c197b90b554882980b13e9
mmgmp <103284514+mmgmp@users.noreply.github.com>
2024-04-12 20:34:17 +0200

    Update sway.sh

--- sway.sh
+++ sway.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-cd ..
 
 # Instalar Sway para Debian 12
 sudo apt install -y sway
 
 # Instalar programas adicionales
-sudo apt install -y waybar tofi firefox-esr lf unzip
+sudo apt install -y waybar tofi firefox-esr lf unzip network-manager wireplumber pipewire-pulse brightnessctl pulseaudio-utils
 
-# Descargar dotfiles de github
-git clone https://github.com/mmgmp/dotfiles
+# Crear carpetas del usuario
+xdg-user-dirs-update
 
-# Colocar archivos config
-mkdir -p ~/.config/sway
-cp dotfiles/.config/sway/config ~/.config/sway/config
-https://use.fontawesome.com/releases/v5.15.4/fontawesome-free-5.15.4-desktop.zip
 # Activar audio
-sudo apt install -y wireplumber pipewire-pulse
 systemctl --user --now enable pipewire pipewire-pulse
 systemctl --user --now enable wireplumber.service
+
+# Activar flatpak y configurar flathub
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+cd ~/Descargas/
 
 # Descargar e instalar font awesome
 wget https://use.fontawesome.com/releases/v5.15.4/fontawesome-free-5.15.4-desktop.zip
@@ -25,5 +25,17 @@ unzip fontawesome-free-5.15.4-desktop.zip
 rm fontawesome-free-5.15.4-desktop.zip
 mkdir -p ~/.local/share/fonts
 cp fontawesome-free-5.15.4-desktop/otfs/*.otf ~/.local/share/fonts
-rm fontawesome-free-5.15.4-desktop
+rm -r fontawesome-free-5.15.4-desktop
 fc-cache -f -v
+
+# Descargar dotfiles de github
+wget https://github.com/mmgmp/dotfiles
+
+# Colocar archivos config
+mkdir -p ~/.config/sway
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/tofi
+
+cp dotfiles/.config/sway/config ~/.config/sway/config
+cp dotfiles/.config/waybar/* ~/.config/waybar/
+cp dotfiles/.config/sway/tofi ~/.config/tofi/config
