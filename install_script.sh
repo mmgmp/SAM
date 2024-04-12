6c838895ae73dc6925fbf2c94f2352d59131f42c
mmgmp <103284514+mmgmp@users.noreply.github.com>
2024-04-13 00:04:51 +0200

    corregir errores

--- install_script.sh
+++ install_script.sh
@@ -8,7 +8,6 @@ make
 sudo make install installsystemd
 sudo systemctl enable ly.service
 cd ..
-rm -f ly
 
 # Instalar Sway para Debian 12
 sudo apt install -y sway
@@ -27,24 +26,20 @@ systemctl --user --now enable wireplumber.service
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-cd ~/Descargas/
-
 # Descargar e instalar font awesome
 wget https://use.fontawesome.com/releases/v5.15.4/fontawesome-free-5.15.4-desktop.zip
 unzip fontawesome-free-5.15.4-desktop.zip
-rm fontawesome-free-5.15.4-desktop.zip
 mkdir -p ~/.local/share/fonts
 cp fontawesome-free-5.15.4-desktop/otfs/*.otf ~/.local/share/fonts
-rm -r fontawesome-free-5.15.4-desktop
 fc-cache -f -v
 
 # Descargar fondo de escritorio
-wget https://github.com/mmgmp/fondos-de-pantalla/blob/main/pixel-paisaje.png?raw=true
+git clone https://github.com/mmgmp/fondos-de-pantalla
 mkdir -p ~/Imágenes/Fondos
-mv pixel-paisaje.png?raw=true ~/Imágenes/Fondos/pixel-paisaje.png
+cp /fondos-de-pantalla/pixel-paisaje.png ~/Imágenes/Fondos/pixel-paisaje.png
 
 # Descargar dotfiles de github
-wget https://github.com/mmgmp/dotfiles
+git clone https://github.com/mmgmp/dotfiles
 
 # Colocar archivos config
 mkdir -p ~/.config/sway
@@ -57,8 +52,7 @@ mkdir -p ~/.local/share/gnupg
 
 cp dotfiles/.config/sway/config ~/.config/sway/config
 cp dotfiles/.config/waybar/* ~/.config/waybar/
-cp dotfiles/.config/sway/tofi ~/.config/tofi/config
+cp dotfiles/.config/tofi/config ~/.config/tofi/config
 cp dotfiles/.config/newsboat/config ~/.config/newsboat/config
 cp dotfiles/.config/nvim ~/.config/nvim
-
 cp dotfiles/.bashrc ~/.bashrc
