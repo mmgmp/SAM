a89d2bae1cf8ca5e26a5e6034c41b077cf619ee5
mmgmp <103284514+mmgmp@users.noreply.github.com>
2024-04-12 21:12:09 +0200

    único instalador

--- auto-cpufreq.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/AdnanHodzic/auto-cpufreq.git
-cd auto-cpufreq
-sudo ./auto-cpufreq-installer
-sudo auto-cpufreq --install
--- install_script.sh
+++ install_script.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Instalar y activar ly
+sudo apt install -y build-essential libpam0g-dev libxcb-xkb-dev
+git clone --recurse-submodules https://github.com/fairyglade/ly
+cd ly/
+make
+sudo make install installsystemd
+sudo systemctl enable ly.service
+cd ..
+rm -f ly
+
 # Instalar Sway para Debian 12
 sudo apt install -y sway
 
 # Instalar programas adicionales
-sudo apt install -y waybar tofi firefox-esr lf unzip network-manager wireplumber pipewire-pulse brightnessctl pulseaudio-utils nala swayidle swaylock mpv newsboat
+sudo apt install -y waybar tofi firefox-esr lf unzip network-manager wireplumber pipewire-pulse brightnessctl pulseaudio-utils nala swayidle swaylock mpv newsboat pass neovim abook
 
 # Crear carpetas del usuario
 xdg-user-dirs-update
@@ -28,6 +38,11 @@ cp fontawesome-free-5.15.4-desktop/otfs/*.otf ~/.local/share/fonts
 rm -r fontawesome-free-5.15.4-desktop
 fc-cache -f -v
 
+# Descargar fondo de escritorio
+wget https://github.com/mmgmp/fondos-de-pantalla/blob/main/pixel-paisaje.png?raw=true
+mkdir -p ~/Imágenes/Fondos
+mv pixel-paisaje.png?raw=true ~/Imágenes/Fondos/pixel-paisaje.png
+
 # Descargar dotfiles de github
 wget https://github.com/mmgmp/dotfiles
 
@@ -35,8 +50,15 @@ wget https://github.com/mmgmp/dotfiles
 mkdir -p ~/.config/sway
 mkdir -p ~/.config/waybar
 mkdir -p ~/.config/tofi
+mkdir -p ~/.config/newsboat
+mkdir -p ~/.config/nvim
+mkdir -p ~/.local/state/bash
+mkdir -p ~/.local/share/gnupg
 
 cp dotfiles/.config/sway/config ~/.config/sway/config
 cp dotfiles/.config/waybar/* ~/.config/waybar/
 cp dotfiles/.config/sway/tofi ~/.config/tofi/config
+cp dotfiles/.config/newsboat/config ~/.config/newsboat/config
+cp dotfiles/.config/nvim ~/.config/nvim
+
 cp dotfiles/.bashrc ~/.bashrc
