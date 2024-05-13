08ee2c982ec20b2950396a568e5ae8c85f310f49
Marcos <103284514+mmgmp@users.noreply.github.com>
2024-05-13 21:46:33 +0200

    add pinentry-gnome3

--- sam.sh
+++ sam.sh
@@ -28,7 +28,7 @@ sudo apt install -y mpv brightnessctl swayimg
 sudo apt install -y sway-notification-center libnotify-bin
 
 # Instalar programas adicionales
-sudo apt install -y unzip curl network-manager newsboat pass pass-otp neovim abook
+sudo apt install -y unzip curl network-manager newsboat pass pass-otp pinentry-gnome3 neovim abook
 
 # Instalar PipeWire
 sudo apt install -y pipewire pulseaudio-utils pavucontrol
