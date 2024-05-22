327b48c7b18a4cd9481ec92577cb0d66fe3bed16
Marcos <103284514+mmgmp@users.noreply.github.com>
2024-05-22 23:38:50 +0200

    installpkg

--- sam.sh
+++ sam.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
+installpkg() {
+	sudo apt install -y "$@" >/dev/null 2>&1
+}
+
 # Instalar sway y complementos
-sudo apt install -y sway swayidle swaybg swaylock waybar xwayland
+installpkg sway swayidle swaybg swaylock waybar xwayland
 
 # Sway autotiling
-sudo apt install -y python3-i3ipc
+installpkg python3-i3ipc
 wget https://raw.githubusercontent.com/nwg-piotr/autotiling/master/autotiling/main.py
 mv main.py autotiling
 chmod +x autotiling
 sudo mv autotiling /usr/bin/autotiling
 
 # Menu de arranque
-sudo apt install -y tofi
+installpkg tofi
 
 # Navegador
-sudo apt install -y firefox-esr
+installpkg firefox-esr
 
 # Gestor de archivos
-sudo apt install -y lf
+installpkg lf
 
 # Multimedia
-sudo apt install -y mpv brightnessctl swayimg
+installpkg mpv brightnessctl swayimg
 
 # Notificaciones
-sudo apt install -y sway-notification-center libnotify-bin
+installpkg sway-notification-center libnotify-bin
 
 # Programas adicionales
-sudo apt install -y unzip zip curl network-manager newsboat pass pass-otp pinentry-gnome3 neovim abook htop bash-completion udisks2
+installpkg unzip zip curl network-manager newsboat pass pass-otp pinentry-gnome3 neovim abook htop bash-completion udisks2
 
 # Instalar PipeWire
-sudo apt install -y pipewire pulseaudio-utils pavucontrol
+installpkg pipewire pulseaudio-utils pavucontrol
 systemctl --user --now enable wireplumber.service
 
 # Instalar flatpak y añadir flathub.com
-sudo apt install -y flatpak
+installpkg flatpak
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Descargar Font Awesome 5
@@ -43,16 +47,16 @@ unzip fontawesome-free-5.15.4-desktop.zip
 cp fontawesome-free-5.15.4-desktop/otfs/*.otf ~/.local/share/fonts
 
 # Descargar FiraMono Nerd Font
-mkdir FiraMonoNerdFont
+mkdir FiraMono
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraMono.zip
-unzip FiraMono.zip -d FiraMonoNerdFont
-cp FiraMonoNerdFont/Fira*.otf ~/.local/share/fonts
+unzip FiraMono.zip -d FiraMono
+cp FiraMono/Fira*.otf ~/.local/share/fonts
 
 # Descargar UbuntuMono Nerd Font
-mkdir UbuntuMonoNerdFont
+mkdir UbuntuMono
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/UbuntuMono.zip
-unzip UbuntuMono.zip -d UbuntuMonoNerdFont
-cp UbuntuMonoNerdFont/Ubuntu*.ttf ~/.local/share/fonts
+unzip UbuntuMono.zip -d UbuntuMono
+cp UbuntuMono/Ubuntu*.ttf ~/.local/share/fonts
 
 # Actualizar cache de fuentes
 fc-cache -f -v
