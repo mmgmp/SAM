fbec9645c2c75576c3a6a3f6072ae26893267b84
Marcos <103284514+mmgmp@users.noreply.github.com>
2024-08-01 07:57:09 +0200

    no borrar

--- sam.sh
+++ sam.sh
@@ -100,8 +100,4 @@ printf 'pinentry-program /usr/bin/pinentry-gnome3' > ~/.local/share/gpg-agent.co
 # Crear carpetas del usuario
 xdg-user-dirs-update
 
-# Limpiar restos
-cd ~/
-sudo rm -r --interactive=never SAM/
-
 printf "\nCompletado, puedes reiniciar el ordenador con el comando 'systemctl reboot'\n"
