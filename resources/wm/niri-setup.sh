05f98889508a5392a6e2726103729826d2bf68a2
mmgmp <marcosmikolta@tutamail.com>
2026-01-26 22:47:40 +0100

    instalacion niri completada y expandida

--- resources/wm/niri-setup.sh
+++ resources/wm/niri-setup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+# Comprobar si las herramientas están instaladas
+[ -f /usr/bin/7z ] || sudo apt install -y p7zip-full
+[ -f /usr/bin/cargo ] || sudo apt install -y cargo
+
 # Instalar las dependencias
 sudo apt install -y gcc clang libudev-dev libgbm-dev libxkbcommon-dev libegl1-mesa-dev libwayland-dev libinput-dev libdbus-1-dev libsystemd-dev libseat-dev libpipewire-0.3-dev libpango1.0-dev libdisplay-info-dev
-
-# Instalar cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Descargar niri v25.11
 wget https://github.com/YaLTeR/niri/archive/refs/tags/v25.11.zip
 
 # Compilar niri
-7z x v25.11.zip ; cd niri-25.11
+7z x v25.11.zip && cd niri-25.11
 cargo build --release
+
+# Crear carpetas
+[ -d /usr/share/wayland-sessions/ ] || sudo mkdir /usr/share/wayland-sessions/
+[ -d /usr/share/xdg-desktop-portal/ ] || sudo mkdir /usr/share/xdg-desktop-portal/
 
 # Mover los archivos
 sudo cp target/release/niri /usr/bin/
@@ -20,5 +25,15 @@ sudo cp resources/niri.desktop /usr/share/wayland-sessions/
 sudo cp resources/niri-portals.conf /usr/share/xdg-desktop-portal/
 sudo cp resources/niri.service /etc/systemd/user/
 sudo cp resources/niri-shutdown.target /etc/systemd/user/
+
+# Herramientas de escritorio
+sudo apt install -y mako waybar swaylock swayidle swaybg
+
+# Portales
+sudo apt install -y xdg-desktop-portal-gtk gnome-keyring
+sudo apt install --no-install-recommends -y xdg-desktop-portal-gnome
+
+# Aplicaciones GTk oscuras
+dconf write /org/gnome/desktop/interface/color-scheme '"prefer-dark"'
 
 cd ..
