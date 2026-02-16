5888bb47299460344d9feac56c46f16eb692e9b9
mmgmp <marcosmikolta@tutamail.com>
2026-02-16 13:01:32 +0100

    ly 1.3.2

--- res/std/ly.sh
+++ res/std/ly.sh
@@ -12,8 +12,9 @@ wget https://ziglang.org/download/0.15.2/zig-x86_64-linux-0.15.2.tar.xz
 7z x zig-x86_64-linux-0.15.2.tar
 
 # Descargar ly
-wget https://codeberg.org/fairyglade/ly/archive/v1.2.0.zip
-7z x v1.2.0.zip && cd ly
+VERSION="1.3.2"
+wget https://codeberg.org/fairyglade/ly/archive/v${VERSION}.zip
+7z x v${VERSION}.zip && cd ly
 
 # Compilar y activar servicio de systemd
 sudo ../zig-x86_64-linux-0.15.2/zig build installexe 
@@ -22,6 +23,5 @@ sudo systemctl enable ly.service
 sudo sed -i 's/clear_password = false/clear_password = true/' /etc/ly/config.ini
 sudo sed -i 's/lang = en/lang = es/' /etc/ly/config.ini
 sudo sed -i 's/numlock = false/numlock = true/' /etc/ly/config.ini
-sudo sed -i 's/session_log = ly-session.log/session_log = null/' /etc/ly/config.ini
 
 cd ..
