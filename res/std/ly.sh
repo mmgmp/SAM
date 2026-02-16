8fa4d7b682b45794f9c206ec3492ca9c5191cc93
mmgmp <marcosmikolta@tutamail.com>
2026-02-16 12:11:55 +0100

    configurar ly

--- res/std/ly.sh
+++ res/std/ly.sh
@@ -19,4 +19,9 @@ wget https://codeberg.org/fairyglade/ly/archive/v1.2.0.zip
 sudo ~/SAM/zig-x86_64-linux-0.15.2/zig build installexe 
 sudo systemctl enable ly.service
 
+sudo sed -i 's/clear_password = false/clear_password = true/' /etc/ly/config.ini
+sudo sed -i 's/lang = en/lang = es/' /etc/ly/config.ini
+sudo sed -i 's/numlock = false/numlock = true/' /etc/ly/config.ini
+sudo sed -i 's/session_log = ly-session.log/session_log = null/' /etc/ly/config.ini
+
 cd ..
