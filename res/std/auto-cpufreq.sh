863ef850854cee4cffb964fd27c9249efc567249
mmgmp <marcosmikolta@tutamail.com>
2026-02-18 13:02:43 +0100

    auto cpu-freq git version

--- res/std/auto-cpufreq.sh
+++ res/std/auto-cpufreq.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+[ -f /usr/bin/git ] || sudo apt install -y git
+
 # Descarga e instala auto-cpufreq
-wget https://github.com/AdnanHodzic/auto-cpufreq/archive/refs/tags/v2.6.0.zip
-7z x auto-cpufreq-2.6.0.zip && cd auto-cpufreq-2.6.0
-sudo ./auto-cpufreq-installer
+git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+cd auto-cpufreq && sudo ./auto-cpufreq-installer
+
 sudo auto-cpufreq --install
 
 cd ..
